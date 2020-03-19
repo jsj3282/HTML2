d16cb127d29071db1118dbaae47f441ce1b14c79
jsj <jeongseonju15@gmail.com>
2020-03-19 17:55:31 +0900

    java script

---
+++ ex04.js
@@ -0,0 +1,7 @@
+
+		var num = 0;
+		console.log(num)
+		document.write("head 태그 내 실행 순서 : " + num + "<br>") /* 에러가 발생하면 그와 연관된 나머지는 모두 실행 안됨 */
+		var age = 23;
+		document.write("<div style='color:red; font-size:24px;'>" + "자바 스크립트</div>")
+	
